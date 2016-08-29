939f4ccefbdd01af6650c4fafeadbce4912ae871
Fetdressing <simon.home@hotmail.com>
2016-08-29 17:13:17 +0200

    reflyttt

---
+++ ProjectPlatformGrappling/Assets/OurStuff/Scripts/ObjectEffects/HealthPickup.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickup : MonoBehaviour {
+    public int healthWorth = 100;
+
+    public float respawnTime = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.transform.GetComponent<Health>() != null)
+        {
+            col.transform.GetComponent<Health>().AddHealth(healthWorth);
+            Die();
+        }
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTime);
+    }
+
+    void Spawn()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    void Die()
+    {
+        if(respawnTime > 0.0f)
+        {
+            this.gameObject.SetActive(false);
+            StartCoroutine(Respawn());
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
