0b93090a7eabdaa4710c3149768d240ff8f864f3
Fetdressing <simon.home@hotmail.com>
2016-09-28 17:50:26 +0200

    meck med respawner objekt

---
+++ ProjectPlatformGrappling/Assets/OurStuff/Scripts/ObjectEffects/ObjectSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : BaseClass {
+    public Transform spawnObject;
+    public Transform spawnPoint;
+
+    public GameObject particleEffect;
+	// Use this for initialization
+	void Start () {
+        Init();
+	}
+
+    public override void Init()
+    {
+        base.Init();
+        if(spawnPoint == null)
+        {
+            spawnPoint = transform;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject parTemp = GameObject.Instantiate(particleEffect.gameObject);
+        parTemp.transform.position = spawnObject.position;
+        Destroy(parTemp, 3);
+
+        spawnObject.position = spawnPoint.position;
+
+        GameObject parTemp2 = GameObject.Instantiate(particleEffect.gameObject);
+        parTemp.transform.position = spawnObject.position;
+        Destroy(parTemp, 3);
+    }
+}
