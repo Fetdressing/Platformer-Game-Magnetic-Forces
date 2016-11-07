d0b0d98b0f77dcee09dbe2568e2f98eb084e29cb
Fetdressing <simon.home@hotmail.com>
2016-11-07 23:44:33 +0100

    rounding

--- ProjectPlatformGrappling/Assets/OurStuff/Scripts/Game/GameManager.cs
+++ ProjectPlatformGrappling/Assets/OurStuff/Scripts/Game/GameManager.cs
@@ -5,16 +5,29 @@ public class GameManager : MonoBehaviour {
     public GameObject gameUIPanel;
     public GameObject menuUIPanel;
     public GameObject settingsUIPanel;
+
+    private Transform player;
+    public Transform goal;
+    public GameObject goalDisplay; //aktiveras när man går i mål
 	// Use this for initialization
 	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        goalDisplay.SetActive(false);
         ToggleMenu(false);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 	    if(Input.GetKeyDown(KeyCode.Escape))
         {
             ToggleMenu();
+        }
+
+        if(Vector3.Distance(player.position, goal.position) < 45)
+        {
+            goalDisplay.SetActive(true);
+            Time.timeScale = 0;
         }
 	}
 
@@ -59,8 +72,13 @@ public class GameManager : MonoBehaviour {
 
     public void ToggleUI(GameObject uiM)
     {
-        if (uiM.gameObject == gameUIPanel.gameObject) gameUIPanel.SetActive(true); else gameUIPanel.SetActive(false);
+        if (uiM.gameObject == gameUIPanel.gameObject) { gameUIPanel.SetActive(true); Cursor.visible = false; } else { gameUIPanel.SetActive(false); Cursor.visible = true; }
         if (uiM.gameObject == settingsUIPanel.gameObject) settingsUIPanel.SetActive(true); else settingsUIPanel.SetActive(false);
         if (uiM.gameObject == menuUIPanel.gameObject) menuUIPanel.SetActive(true); else menuUIPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
